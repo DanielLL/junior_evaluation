dcb734e0854f6e54a6ba71d9a4a42d7ff1705edc
DanielLL <daniel.lopez@tangosource.com>
2013-12-03 11:33:32 -0600

    Added basic structure for the exercise 4

---
+++ exercise_4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exercise4::Application.config.secret_key_base = '762b678fc815e649e93ad9e86457f005f8078459baba6849df755b9f3a6262fe4ddb3e89737e5134c3125e2b169ce6c8699fd7b355d7735bfe7bcafa19ec3e6c'
---
+++ exercise_4/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Exercise4::Application.config.session_store :cookie_store, key: '_exercise_4_session'
